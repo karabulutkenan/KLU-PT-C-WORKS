68e589bee25d2a16cd53905f7c9d7aa49087e21b
Kenan Karabulut <71269248+karabulutkenan@users.noreply.github.com>
2021-01-25 16:51:01 +0300

    Update Soru9CevapDizi.c

--- Soru9CevapDizi.c
+++ Soru9CevapDizi.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 
-int main() {
+main() {
 
-  int dizi[10];
-  float islem;
-  float toplam=0;
-  int i;
+  int dizi[10],i,toplam=0,ort;
   
-  for (i=1;i<=10;i++)
+  for (i=0;i<=9;i++)
   {
-  	printf("%d. Sayiyi Giriniz: ",i);
+  	printf("%d. Sayiyi Giriniz: ",i+1);
   	scanf("%d",&dizi[i]);
   	toplam=toplam+dizi[i];
-  	islem=toplam/i;
   }
-
-  printf("\n BASTAN SONA DOGRU: %d,%d,%d,%d,%d,%d,%d,%d,%d,%d,\n", dizi[1], dizi[2], dizi[3], dizi[4], dizi[5], dizi[6], dizi[7], dizi[8], dizi[9],dizi[10]);
-  printf("\n SONDAN BASA DOGRU: %d,%d,%d,%d,%d,%d,%d,%d,%d,%d,\n",dizi[10], dizi[9], dizi[8], dizi[7], dizi[6], dizi[5], dizi[4], dizi[3], dizi[2], dizi[1]);
-  printf("\n Ortalama: %.2f",islem);
-  
-
+  for (i=0;i<=9;i++)
+  {
+  	printf("%d\n ",dizi[i]);
+  	
+  }
+  for (i=9;i>=0;i--)
+  {
+  	printf("%d\n ",dizi[i]);
+  	
+  }
+  ort=toplam/10;
+  printf("\n Ortalama: %d",ort);
  
-	
-	return 0;
 }
