65880236b517c0135c625ea1e0b5a66349b2ebe6
Kenan Karabulut <71269248+karabulutkenan@users.noreply.github.com>
2021-01-25 16:22:00 +0300

    Update Soru7CevapFonk.c

--- Soru7CevapFonk.c
+++ Soru7CevapFonk.c
@@ -1,25 +1,14 @@
 #include <stdio.h>
-
-int main() {
-
-  int dizi[10];
-  float islem;
-  int toplam=0;
+#include <math.h>
+main()
+{
+	int x,n,fx=0;
+	printf("Sayi Giriniz: ");
+	scanf("%d",&n);
+	for(x=1; x<=n; x++);
+	{
+		fx=fx+pow(x,2);	
+	}
+	printf("Sonuc: %d",fx);
   
-  for (int i=0;i<=10;i++)
-  {
-  	printf("%d. Sayiyi Giriniz: ",i);
-  	scanf("%d",&dizi);
-  	toplam=toplam+dizi;
-  	islem=toplam/n;
-  }
-
-  printf(dizi[0], dizi[1], dizi[2], dizi[3], dizi[4], dizi[5], dizi[6], dizi[7], dizi[8], dizi[9]),\n;
-  printf(dizi[0], dizi[1], dizi[2], dizi[3], dizi[4], dizi[5], dizi[6], dizi[7], dizi[8], dizi[9]), \n;
-  printf("Ortalama: %d",islem);
-  
-
- 
-	
-	return 0;
 }
